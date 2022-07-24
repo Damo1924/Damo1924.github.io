8c72f4e04a8b1d847465a9bb00dd08e1466a2813
Damo1924 <88201512+Damo1924@users.noreply.github.com>
2022-07-24 14:53:05 +0900

    Update 1D_SegmentTree_BottomUp.cpp

--- _posts/Structures/1D_SegmentTree_BottomUp.cpp
+++ _posts/Structures/1D_SegmentTree_BottomUp.cpp
@@ -1,32 +1,23 @@
 struct Segtree {
     int n;
     vector<int> node, a;
-    
     Segtree() {}
-    Segtree(int _n) : n(_n)
-    {
+    Segtree(int _n) : n(_n) {
         node.resize(2 * n);
         a.resize(n);
     }
-    
-    void build()
-    {
+    void build() {
         for (int i = 0; i < n; i++) node[n + i] = a[i];
         for (int i = n - 1; i; i--) node[i] = node[2 * i] + node[2 * i + 1];
     }
-    
-    void upd(int idx, int val)
-    {
+    void upd(int idx, int val) {
         for (int i = n + idx; i; i /= 2) node[i] += val - a[idx];
         a[idx] = val;
     }
-    
-    int query(int l, int r)
-    {
+    int query(int l, int r) {
         l += n; r += n;
         int res = 0;
-        while (l <= r)
-        {
+        while (l <= r) {
             if (l % 2 != 0) res += node[l++];
             if (r % 2 == 0) res += node[r--];
             l /= 2; r /= 2;
